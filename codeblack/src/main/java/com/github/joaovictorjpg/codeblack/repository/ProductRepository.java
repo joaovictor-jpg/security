2bb99d89759e8a2938af8d4e217cf30d01ef2d81
joao victor <joaovictormdasilva676@gmail.com>
2023-04-09 20:07:36 -0300

    Created a new repository productRepository

---
+++ codeblack/src/main/java/com/github/joaovictorjpg/codeblack/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.github.joaovictorjpg.codeblack.repository;
+
+import com.github.joaovictorjpg.codeblack.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByTitle(String title);
+    Product deleteByProductId(Long productId);
+    Optional<Product> findByCategoryId(Long categoryId);
+}
