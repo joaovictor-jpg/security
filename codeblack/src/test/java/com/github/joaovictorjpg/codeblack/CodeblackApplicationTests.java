ff2a62d6ba8c1be5e357d339351badc3aa08160d
joao victor <joaovictormdasilva676@gmail.com>
2023-04-09 13:45:17 -0300

    Create a new project coffee shop

---
+++ codeblack/src/test/java/com/github/joaovictorjpg/codeblack/CodeblackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaovictorjpg.codeblack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeblackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
