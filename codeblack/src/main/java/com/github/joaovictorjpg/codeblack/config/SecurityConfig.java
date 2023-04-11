022a84dbaf087ef168a2b2aadf9fdc7336b3f4fc
joao victor <joaovictormdasilva676@gmail.com>
2023-04-11 20:57:20 -0300

    Created the package configuration and the securityConfig class. Authorized layer created

---
+++ codeblack/src/main/java/com/github/joaovictorjpg/codeblack/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.github.joaovictorjpg.codeblack.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests(
+                        auth -> {
+                            auth.requestMatchers(HttpMethod.GET, "/api/product").permitAll();
+                            auth.anyRequest().authenticated();
+                        }
+                ).build();
+
+    }
+
+}
