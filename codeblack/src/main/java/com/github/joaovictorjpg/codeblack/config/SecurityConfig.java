9b30b8cf14f4a6928e884fb08edd63c87e91645f
joao victor <joaovictormdasilva676@gmail.com>
2023-04-16 00:40:41 -0300

    Config user context and update securityConfig

--- codeblack/src/main/java/com/github/joaovictorjpg/codeblack/config/SecurityConfig.java
+++ codeblack/src/main/java/com/github/joaovictorjpg/codeblack/config/SecurityConfig.java
@@ -1,19 +1,29 @@
 package com.github.joaovictorjpg.codeblack.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationFilter filter;
+
+    @Autowired
+    private AuthenticationProvider provider;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,11 +32,19 @@ public class SecurityConfig {
                 .disable()
                 .authorizeHttpRequests(
                         auth -> {
-                            auth.requestMatchers(HttpMethod.GET, "/api/product").permitAll();
-                            auth.requestMatchers(HttpMethod.POST, "/api/users/post/**").permitAll();
+                            auth.requestMatchers(HttpMethod.GET, "/api/product")
+                                    .permitAll();
+                            auth.requestMatchers(HttpMethod.POST, "/api/users/post/**")
+                                    .hasAuthority("STAFF");
                             auth.anyRequest().authenticated();
                         }
-                ).build();
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(provider)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
 
