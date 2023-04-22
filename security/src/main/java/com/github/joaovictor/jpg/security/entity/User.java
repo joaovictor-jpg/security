7cd796ca665592dcb402421639b6e87daf533388
joao victor <joaovictormdasilva676@gmail.com>
2023-04-22 20:10:21 -0300

    turn user class into an entity

--- security/src/main/java/com/github/joaovictor/jpg/security/entity/User.java
+++ security/src/main/java/com/github/joaovictor/jpg/security/entity/User.java
@@ -1,5 +1,6 @@
 package com.github.joaovictor.jpg.security.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
