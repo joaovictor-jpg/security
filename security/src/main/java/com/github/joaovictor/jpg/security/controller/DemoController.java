49bdf38513d573f26ee55a4fb87208a712f9654a
joao victor <joaovictormdasilva676@gmail.com>
2023-04-23 17:50:29 -0300

    Create a new controller class and a whiteList

---
+++ security/src/main/java/com/github/joaovictor/jpg/security/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.github.joaovictor.jpg.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok().body("Hello from security endpoint");
+    }
+}
