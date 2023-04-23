9a820dbd7b10bb68e4e6d0ae8c49120f584e2e56
joao victor <joaovictormdasilva676@gmail.com>
2023-04-22 21:45:01 -0300

    Created a method doFilterInternal

--- security/src/main/java/com/github/joaovictor/jpg/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/github/joaovictor/jpg/security/config/JwtAuthenticationFilter.java
@@ -20,5 +20,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if(authHeader.isEmpty() || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
     }
 }
